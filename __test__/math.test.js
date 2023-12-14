7053dc14d90e2fddbbe6c278a7b909bb7e0a953b
adrianortzs <adrianortizsuarez@icloud.com>
2023-12-14 11:13:25 +0100

    update

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -6,10 +6,10 @@ test('Adding 1 + 1 equals 2', () => {
 test('Multiplying 1 * 1 equals 1', () => {
   expect(mul(1, 1)).toBe(1);
 });
-test('Subtracting 1 - 1 equals 0', () => {
+test('Subtracting 1 - 1 equals 0', (2) => {
   expect(sub(1, 1)).toBe(0);
 });
-test('Dividing 1 / 1 equals 1', () => {
+test('Dividing 1 / 1 equals 1', (10000) => {
   expect(div(1, 1)).toBe(1);
 });
 test('Dividing 1/0 equals infinite',()=>{

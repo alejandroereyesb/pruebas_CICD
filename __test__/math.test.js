b0c176fa47e9faa1154dd02086392332bd81716c
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-05-09 11:44:56 +0200

    add/test_error

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -4,13 +4,13 @@ test('Adding 1 + 1 equals 2', () => {
   expect(sum(1, 1)).toBe(2);
 });
 test('Multiplying 1 * 1 equals 1', () => {
-  expect(mul(1, 1)).toBe(1);
+  expect(mul(1, 1)).toBe(666);
 });
 test('Subtracting 1 - 1 equals 0', () => {
   expect(sub(1, 1)).toBe(0);
 });
 test('Dividing 1 / 1 equals 1', () => {
-  expect(div(1, 1)).toBe(1);
+  expect(div(1, 1)).toBe(333);
 });
 test('Dividing 1/0 equals infinite',()=>{
   expect(div(1, 0)).toBe(Infinity);
